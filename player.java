f105b0cb88a61b904e34e8a8023043d0e655765e
Tejashri3 <55490938+Tejashri3@users.noreply.github.com>
2020-03-03 22:15:26 +0000

    Create player.java

---
+++ player.java
@@ -0,0 +1,15 @@
+/*
+* This class contains setter and getter methods of Player's name
+*/
+
+public class player {
+    private String name;
+
+    public void setName(String inputName) {
+        name = inputName;
+    }
+
+    public String getName() {
+    return name;
+    }
+}
